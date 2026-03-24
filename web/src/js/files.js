4dc05c8b77714bf6414ae73ac4a8ddc1cdc394b8
devFernandoS <devtoolsfernando@gmail.com>
2026-03-24 20:02:59 -0300

    habilitar descarga cv en Ingles

--- web/src/js/files.js
+++ web/src/js/files.js
@@ -2,8 +2,23 @@
 document.getElementById("btnDescargarCVes").addEventListener("click", function () {
    try {
       const link = document.createElement("a");
-      link.href = "/CV_Sullca_Fernando_FullStack.pdf"; // archivo ubicado en web/public/cv.pdf
-      link.download = "CV_Willian_Fernando_Sullca.pdf";
+      link.href = "/CV_Willian Fernando_Sullca_FS.pdf"; // archivo ubicado en web/public/cv.pdf
+      link.download = "CV_Willian_Fernando_Sullca_FullStack.pdf";
+      link.click();
+   } catch (error) {
+      console.log("Error descarga CV:");
+      console.error("Error al descargar el CV:", error);
+   }
+
+});
+
+
+
+document.getElementById("btnDescargarCVen").addEventListener("click", function () {
+   try {
+      const link = document.createElement("a");
+      link.href = "/CV_Willian Fernando_Sullca_FullStack_EN.pdf"; // archivo ubicado en web/public/cv.pdf
+      link.download = "CV_Willian_Fernando_Sullca_FullStack_EN.pdf";
       link.click();
    } catch (error) {
       console.log("Error descarga CV:");
