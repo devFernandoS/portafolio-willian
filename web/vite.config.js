9551d0b7725f9880b0b17970c4617266c9c20bdc
devFernandoS <devtoolsfernando@gmail.com>
2026-03-30 19:38:49 -0300

    fix build y preview en local + vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,27 +3,27 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // El directorio raíz ahora es 'web' (donde está este archivo), que es el comportamiento por defecto.
-  // Ya no usamos la opción 'root: 'src''.
 
-  // 'publicDir' es ahora relativo a la raíz 'web'.
-  publicDir: 'public',
+  root: 'src',           // Vite opera desde web/src/
+  publicDir: '../public',
 
   build: {
-    // 'outDir' es ahora relativo a la raíz 'web'.
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
-    // Usamos rutas absolutas para eliminar cualquier ambigüedad en el entorno de build.
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        contacto: resolve(__dirname, 'src/contacto.html'),
-        404: resolve(__dirname, 'src/404.html'),
-        proyectos: resolve(__dirname, 'src/proyectos.html'),
-        desarrollo: resolve(__dirname, 'src/desarrollo.html')
+        //  adelante, SIN 'src/', SIN resolve(__dirname, ...)
+        main: 'src/index.html',
+        contacto: 'src/contacto.html',
+        '404': 'src/404.html',
+        proyectos: 'src/proyectos.html',
+        desarrollo: 'src/desarrollo.html'
       }
     }
   }
 })
+
+
+
 
 
