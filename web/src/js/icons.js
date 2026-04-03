710924917275d1391786a3e3d7e7bdb269dd217f
devFernandoS <devtoolsfernando@gmail.com>
2026-04-03 18:07:07 -0300

    add switcher 'temas'  and icons

---
+++ web/src/js/icons.js
@@ -0,0 +1,13 @@
+import {
+    createIcons, Mail, GitCommitVertical, Code2,
+    Code, Moon, Sun, Stars, ExternalLink,
+    Terminal, Database, Layers
+} from 'lucide';
+
+createIcons({
+    icons: {
+        Mail, Code, GitCommitVertical, Code2,
+        Moon, Sun, Stars, ExternalLink, Terminal,
+        Database, Layers
+    }
+});
