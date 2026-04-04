6647822cb9808a85c5f9e8035d1433600d1f52a7
devFernandoS <devtoolsfernando@gmail.com>
2026-04-04 19:46:50 -0300

    eliminar selector y efectos por js de seccion visitada, agregando control de menus hamburgesa

--- web/src/js/navbar.js
+++ web/src/js/navbar.js
@@ -9,3 +9,22 @@ document.querySelectorAll(".nav-link").forEach(link => {
 
     }
 });
+
+
+// Seleccionamos los elementos
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const iconMenu = document.getElementById('icon-menu');
+// const iconClose = document.getElementById('icon-close');
+
+if (mobileMenuBtn && mobileMenu) {
+    mobileMenuBtn.addEventListener('click', () => {
+        // Alternamos la clase 'hidden' y 'flex' del menú
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+
+        // Alternamos los íconos de hamburguesa (menu) y equis (x)
+        iconMenu.classList.toggle('hidden');
+        // iconClose.classList.toggle('hidden');
+    });
+}
