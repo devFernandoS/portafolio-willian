5fb7d93c9d84b744c6aa1fee724765f90c6308cf
devFernandoS <devtoolsfernando@gmail.com>
2026-04-04 11:17:04 -0300

    update sv

--- web/src/js/files.js
+++ web/src/js/files.js
@@ -2,7 +2,7 @@
 document.getElementById("btnDescargarCVes")?.addEventListener("click", function () {
    try {
       const link = document.createElement("a");
-      link.href = "/CV_Willian Fernando_Sullca_FS.pdf"; // archivo ubicado en web/public/cv.pdf
+      link.href = "/CV_Willian_Sullca_FullStack.pdf"; // archivo ubicado en web/public/cv.pdf"; 
       link.download = "CV_Willian_Fernando_Sullca_FullStack.pdf";
       link.click();
    } catch (error) {
